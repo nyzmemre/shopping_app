43dd6b109e22b129e45f08d8ed8277f2f28883b9
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2021-12-05 23:53:27 +0300

    added product details page with pageview for images

--- lib/utils/my_widgets/product_details.dart
+++ lib/utils/my_widgets/product_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:shopping_app/product_data/olive_oil_data.dart';
+import 'package:shopping_app/utils/constants/extensions/extensions.dart';
 import 'package:shopping_app/utils/constants/texts/product_texts.dart';
 
 class ProductDetails extends StatelessWidget {
@@ -10,10 +11,21 @@ class ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return SafeArea(child: Scaffold(
       body: Column(
         children: [
-          Image.asset(data.oliveOilList[index].imgURL[0]),
+          Expanded(
+            flex: 5,
+            child: PageView.builder(
+              scrollDirection: Axis.horizontal,
+                itemCount: data.oliveOilList[index].imgURL.length,
+                itemBuilder: (context, int index){
+              return SizedBox(
+                  width: context.width*0.5,
+                  child: Image.asset(data.oliveOilList[index].imgURL[index]));
+            }),
+          ),
           Text("Product: "+ data.oliveOilList[index].name),
           Text("\$"+data.oliveOilList[index].price.toString()),
           (data.oliveOilList[index].isAvailable) ? Text(available) : Text(unavailable),
